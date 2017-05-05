43fe36b5695205c10c64580a2b78cfe3cb523ca0
Shuai-Hsun <victory2152@livemail.tw>
2017-05-05 14:40:02 -0700

    select multi-lines and once

--- multiupdown.py
+++ multiupdown.py
@@ -11,9 +11,18 @@ class multi_up(sublime_plugin.TextCommand):
 
 class multi_down(sublime_plugin.TextCommand):
 	def run(self, edit, nlines):
-		screenful = self.view.visible_region()
 		(row, col) = self.view.rowcol(self.view.sel()[0].begin())
 		pt = self.view.text_point(row + nlines, col)
 		self.view.sel().clear()
 		self.view.sel().add(sublime.Region(pt))
 		self.view.show(pt)
+
+class multi_up_select(sublime_plugin.TextCommand):
+	def run(self, edit, nlines):
+		for i in range(nlines):
+			self.view.run_command('move', {"by": "lines", "forward": False, "extend":True})
+		
+class multi_down_select(sublime_plugin.TextCommand):
+	def run(self, edit, nlines):
+		for i in range(nlines):
+			self.view.run_command('move', {"by": "lines", "forward": True, "extend":True})
