aceed8cca464357bd6437c71cb133845fdd35681
Shuai-Hsun <victory2152@livemail.tw>
2017-05-05 16:24:32 -0700

    more simple way to do multi_up and multi_down

--- multiupdown.py
+++ multiupdown.py
@@ -3,19 +3,23 @@ import sublime_plugin
 
 class multi_up(sublime_plugin.TextCommand):
 	def run(self, edit, nlines):
-		(row, col) = self.view.rowcol(self.view.sel()[0].begin())
-		pt = self.view.text_point(row - nlines, 0)
-		self.view.sel().clear()
-		self.view.sel().add(sublime.Region(pt))
-		self.view.show(pt)
+		for i in range(nlines):
+			self.view.run_command('move', {"by": "lines", "forward": False})
+		# (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+		# pt = self.view.text_point(row - nlines, 0)
+		# self.view.sel().clear()
+		# self.view.sel().add(sublime.Region(pt))
+		# self.view.show(pt)
 
 class multi_down(sublime_plugin.TextCommand):
 	def run(self, edit, nlines):
-		(row, col) = self.view.rowcol(self.view.sel()[0].begin())
-		pt = self.view.text_point(row + nlines, col)
-		self.view.sel().clear()
-		self.view.sel().add(sublime.Region(pt))
-		self.view.show(pt)
+		for i in range(nlines):
+			self.view.run_command('move', {"by": "lines", "forward": True})
+		# (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+		# pt = self.view.text_point(row + nlines, col)
+		# self.view.sel().clear()
+		# self.view.sel().add(sublime.Region(pt))
+		# self.view.show(pt)
 
 class multi_up_select(sublime_plugin.TextCommand):
 	def run(self, edit, nlines):
